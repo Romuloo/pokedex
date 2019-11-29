232881bd780c8f64d0e7245fa02e71ed87ed9942
Javier Linares <j.linares5@usp.ceu.es>
2019-11-29 13:19:56 +0100

    Annadido JTabbe

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,12 +7,17 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        PanelMapa p = new PanelMapa();
-        //Stats.start("res/imagenes/fotos/4.png", "Charmander",200,149,101,190,100,130);
-        JFrame f = new JFrame();
-        f.add(p);
-        f.setTitle("Mapa");
 
+        JTabbedPane jt = new JTabbedPane();
+        PanelMapa p = new PanelMapa();
+        JFrame f = new JFrame();
+
+        jt.add(p);
+        //Stats.start("res/imagenes/fotos/4.png", "Charmander",200,149,101,190,100,130);
+
+        f.add(jt);
+
+        f.setTitle("Pokedex");
         f.setSize(((1500/3)*2)+100,(1061/3)*2);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
