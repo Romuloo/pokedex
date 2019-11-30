0f92b61ae4a1d445f751033a9d4507beddd4b1b5
Javier Linares <j.linares5@usp.ceu.es>
2019-11-30 20:00:23 +0100

    Annadido los botones y sus localizaciones

--- src/domain/localizacion/Imagen.java
+++ src/domain/localizacion/Imagen.java
@@ -10,25 +10,17 @@ import java.awt.*;
 public class Imagen extends JPanel {
 
     public Imagen() {
-
         this.setSize((1500/3)*2, (1061/3)*2);
+
     }
 
-
     public void paint(Graphics grafico) {
-
         Dimension height = getSize();
-
         ImageIcon Img = new ImageIcon("res/imagenes/kanto.png");
-
-
         grafico.drawImage(Img.getImage(), 0, 0, height.width, height.height, null);
-
-
-
         setOpaque(false);
-
         super.paint(grafico);
+
     }
 }
 
--- src/domain/localizacion/PanelMapa.java
+++ src/domain/localizacion/PanelMapa.java
@@ -30,26 +30,33 @@ public class PanelMapa  extends JPanel{
 
 
 
-    //JList
-    private void initList(){
 
-        s.setBounds(0,0,100,(1061/3)*2);
-        add(s, BorderLayout.WEST);
 
-    }
-
+    //annade pokemons al modelo de la JList
     private void addPokemons(){
 
         for(int i = 0; i < pokemons().size(); i++) modelo.add(i, pokemons().get(i));
     }
 
+    //elimina pokemons del modelo de la JList
     private void removePokemons(){
         for(int i = pokemons().size() - 1; i >= 0; i--) modelo.remove(i);
     }
 
+    //establece el modelo de la JList
     private void setModelo(){
-        addPokemons();
-        list.setModel(modelo);
+       if(modelo.isEmpty()) addPokemons();
+       else if(!modelo.isEmpty()) removePokemons();
+       list.setModel(modelo);
+    }
+
+    //JList
+    private void initList(){
+        list.setBackground(Color.cyan);
+        s.setBackground(Color.red);
+        s.setBounds(0,0,100,(1061/3)*2);
+        add(s, BorderLayout.WEST);
+
     }
 
     //Panel Imagen
@@ -57,13 +64,33 @@ public class PanelMapa  extends JPanel{
         Imagen i = new Imagen();
         i.setLayout(null);
         i.setBounds(100,0, (1500/3)*2, (1061/3)*2);
-
         //Image icon
         ImageIcon in = new ImageIcon("res/imagenes/icon.png");
         add(i, BorderLayout.CENTER);
+
         JButton b1 = new JButton();
-        i.add(b1, BorderLayout.CENTER);
-        b1.setBounds(0,0, 30,30);
+        JButton b2 = new JButton();
+        JButton b3 = new JButton();
+        JButton b4 = new JButton();
+        JButton b5 = new JButton();
+        JButton b6 = new JButton();
+        JButton b7 = new JButton();
+
+        i.add(b1);
+        i.add(b2);
+        i.add(b3);
+        i.add(b4);
+        i.add(b5);
+        i.add(b6);
+        i.add(b7);
+
+        b1.setBounds(109,207, 30,30);
+        b2.setBounds(430,68, 30,30);
+        b3.setBounds(896,200, 30,30);
+        b4.setBounds(613,68, 30,30);
+        b5.setBounds(374,635, 30,30);
+        b6.setBounds(487,415, 30,30);
+        b7.setBounds(219,190, 30,30);
 
 
         b1.setIcon(new ImageIcon(in.getImage().getScaledInstance(b1.getWidth(), b1.getHeight(), Image.SCALE_SMOOTH)));
@@ -72,17 +99,72 @@ public class PanelMapa  extends JPanel{
         b1.setBorderPainted(false);
         b1.setBorder(null);
 
+        b2.setIcon(new ImageIcon(in.getImage().getScaledInstance(b2.getWidth(), b2.getHeight(), Image.SCALE_SMOOTH)));
+        b2.setOpaque(true);
+        b2.setContentAreaFilled(false);
+        b2.setBorderPainted(false);
+        b2.setBorder(null);
+
+        b3.setIcon(new ImageIcon(in.getImage().getScaledInstance(b3.getWidth(), b3.getHeight(), Image.SCALE_SMOOTH)));
+        b3.setOpaque(true);
+        b3.setContentAreaFilled(false);
+        b3.setBorderPainted(false);
+        b3.setBorder(null);
+
+        b4.setIcon(new ImageIcon(in.getImage().getScaledInstance(b4.getWidth(), b4.getHeight(), Image.SCALE_SMOOTH)));
+        b4.setOpaque(true);
+        b4.setContentAreaFilled(false);
+        b4.setBorderPainted(false);
+        b4.setBorder(null);
+
+        b5.setIcon(new ImageIcon(in.getImage().getScaledInstance(b5.getWidth(), b5.getHeight(), Image.SCALE_SMOOTH)));
+        b5.setOpaque(true);
+        b5.setContentAreaFilled(false);
+        b5.setBorderPainted(false);
+        b5.setBorder(null);
+
+        b6.setIcon(new ImageIcon(in.getImage().getScaledInstance(b6.getWidth(), b6.getHeight(), Image.SCALE_SMOOTH)));
+        b6.setOpaque(true);
+        b6.setContentAreaFilled(false);
+        b6.setBorderPainted(false);
+        b6.setBorder(null);
+
+        b7.setIcon(new ImageIcon(in.getImage().getScaledInstance(b7.getWidth(), b7.getHeight(), Image.SCALE_SMOOTH)));
+        b7.setOpaque(true);
+        b7.setContentAreaFilled(false);
+        b7.setBorderPainted(false);
+        b7.setBorder(null);
+
+
+
+
+
+
         b1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-               if(modelo.isEmpty()) setModelo();
-               else if(!modelo.isEmpty()) removePokemons();
+                setModelo();
+            }
+        });
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setModelo();
+            }
+        });
+        b3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setModelo();
             }
         });
 
     }
 
 
+    /**
+     * Construye un PanelMapa
+     */
 
     public PanelMapa(){
        setLayout(null);
