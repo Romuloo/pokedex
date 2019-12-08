eac850272a2e1ad587b9f139622a5f9636cff2a8
Javier Linares <j.linares5@usp.ceu.es>
2019-12-08 17:34:25 +0100

    Annadida ayuda

--- src/app/before/Begin.java
+++ src/app/before/Begin.java
@@ -12,6 +12,10 @@ public class Begin extends JPanel{
 
     private static JFrame main = new JFrame();
     private JButton start = new JButton();
+    private JButton help = new JButton();
+    private ImageIcon ic = new ImageIcon("res/imagenes/help.png");
+    private ImageIcon flecha = new ImageIcon("res/imagenes/fAmarilla.png");
+    private Icon flechaAmarilla = new ImageIcon(flecha.getImage().getScaledInstance(512/10, 512/10, Image.SCALE_DEFAULT));
 
     /**
      * Construye un panel con sus componentes.
@@ -50,6 +54,24 @@ public class Begin extends JPanel{
             }
         });
         add(start);
+
+        help.setBounds(22,30, 60,60 );
+        help.setIcon(new ImageIcon(ic.getImage().getScaledInstance(help.getWidth(), help.getHeight(), Image.SCALE_SMOOTH)));
+
+        help.setVisible(true);
+        help.setOpaque(true);
+        help.setContentAreaFilled(false);
+        help.setBorderPainted(false);
+        help.setBorder(null);
+        help.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(help, "Pulsa en la flecha amarilla para abrir la Pokedex", "Ayuda" ,1, flechaAmarilla);
+            }
+        });
+        add(help);
+
+
     }
 
     private void begin(){
